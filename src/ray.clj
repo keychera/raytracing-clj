502b79505c03a53003b620bf3edd23bf193c4037
keychera <kyogatama@gmail.com>
2025-10-14 20:25:45 +0900

    interpolate sky, fix missing color calc

--- src/ray.clj
+++ src/ray.clj
@@ -1,4 +1,4 @@
-(ns ray 
+(ns ray
   (:require
    [vec3]))
 
@@ -7,4 +7,8 @@
 (defn at [{::keys [origin direction]} t]
   (vec3/add origin (vec3/multiply direction t)))
 
-(defn color [ray] [0 0 0])
+(defn color [{::keys [direction]}]
+  (let [[_x y _z] (vec3/unit direction)
+        a         (* 0.5 (+ y 1.0))]
+    (vec3/add (vec3/multiply [1.0 1.0 1.0] (- 1.0 a))
+              (vec3/multiply [0.5 0.7 1] a))))
--- src/raytracing.clj
+++ src/raytracing.clj
@@ -7,38 +7,43 @@
 
 ;; following https://raytracing.github.io/books/RayTracingInOneWeekend.html
 
-(defn -main []
-  (let [;; image
-        aspect-ratio    16/9
-        image-width     400
-        image-height    (int (/ image-width aspect-ratio))
+(defn write-color! [out color]
+  (let [[r g b] (mapv #(int (* 255 %)) color)]
+    (.write out (str r " " g " " b "\n"))))
 
-        ;; camera
-        focal-length    1.0
-        viewport-height 2.0
-        ;; not using aspect-ratio is deliberate here
-        viewport-width  (* viewport-height (/ image-width image-height))
-        camera-center   [0 0 0]
-        viewport-u      [viewport-width 0 0]
-        viewport-v      [0 (- viewport-height) 0] ;; negative because we want upper-left to be zero and increases at we scan down
-        pixel-du        (vec3/divide viewport-u image-width)
-        pixel-dv        (vec3/divide viewport-v image-height)
-        upper-left      (-> camera-center
-                            (vec3/subtract [0 0 focal-length])
-                            (vec3/subtract (vec3/divide viewport-u 2))
-                            (vec3/subtract (vec3/divide viewport-v 2)))
-        pixel-00-loc    (vec3/add upper-left (vec3/multiply (vec3/add pixel-du pixel-dv) 0.5))]
-    (with-open [out (io/writer "scene.ppm")]
-      (.write out (str "P3\n" image-width " " image-height "\n255\n"))
-      (doall
-       (for [j (range image-height)
-             i (range image-width)]
-         (let [pixel-center  (-> pixel-00-loc
-                                 (vec3/add (vec3/multiply pixel-du i))
-                                 (vec3/add (vec3/multiply pixel-dv j)))
-               ray-direction (vec3/subtract pixel-center camera-center)
-               a-ray         #::ray{:origin camera-center ::direction ray-direction}
-               [r g b]       (ray/color a-ray)]
-           (.write out (str r " " g " " b "\n"))))))
-    :done))
+(defn -main []
+  (time
+   (let [;; image
+         aspect-ratio    16/9
+         image-width     400
+         image-height    (int (/ image-width aspect-ratio))
+
+         ;; camera
+         focal-length    1.0
+         viewport-height 2.0
+         ;; not using aspect-ratio is deliberate here
+         viewport-width  (* viewport-height (/ image-width image-height))
+         camera-center   [0 0 0]
+         viewport-u      [viewport-width 0 0]
+         viewport-v      [0 (- viewport-height) 0] ;; negative because we want upper-left to be zero and increases at we scan down
+         pixel-du        (vec3/divide viewport-u image-width)
+         pixel-dv        (vec3/divide viewport-v image-height)
+         upper-left      (-> camera-center
+                             (vec3/subtract [0 0 focal-length])
+                             (vec3/subtract (vec3/divide viewport-u 2))
+                             (vec3/subtract (vec3/divide viewport-v 2)))
+         pixel-00-loc    (vec3/add upper-left (vec3/multiply (vec3/add pixel-du pixel-dv) 0.5))]
+     (with-open [out (io/writer "scene.ppm")]
+       (.write out (str "P3\n" image-width " " image-height "\n255\n"))
+       (doall
+        (for [j (range image-height)
+              i (range image-width)]
+          (let [pixel-center  (-> pixel-00-loc
+                                  (vec3/add (vec3/multiply pixel-du i))
+                                  (vec3/add (vec3/multiply pixel-dv j)))
+                ray-direction (vec3/subtract pixel-center camera-center)
+                a-ray         #::ray{:origin camera-center :direction ray-direction}
+                color         (ray/color a-ray)]
+            (write-color! out color)))))
+     :done)))
 
