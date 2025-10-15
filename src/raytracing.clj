2723cc4ee3e58804e0b1d2685e8e55ac81d61286
keychera <kyogatama@gmail.com>
2025-10-16 02:11:00 +0900

    pixel sampling

--- src/raytracing.clj
+++ src/raytracing.clj
@@ -10,8 +10,10 @@
 
 (set! *warn-on-reflection* true)
 
+(defn clamp [x min-v max-v] (min max-v (max x min-v)))
+
 (defn write-color! [^java.io.BufferedWriter out color]
-  (let [[r g b] (mapv #(int (* 255 %)) color)]
+  (let [[r g b] (mapv #(int (* 256 (clamp % 0.0 0.999))) color)]
     (.write out (str r " " g " " b "\n"))))
 
 (def hittables
@@ -60,17 +62,22 @@
                              (vec3/subtract [0 0 focal-length])
                              (vec3/subtract (vec3/divide viewport-u 2))
                              (vec3/subtract (vec3/divide viewport-v 2)))
-         pixel-00-loc    (vec3/add upper-left (vec3/multiply (vec3/add pixel-du pixel-dv) 0.5))]
+         pixel-00-loc    (vec3/add upper-left (vec3/multiply (vec3/add pixel-du pixel-dv) 0.5))
+
+         samples-per-px  10
+         colors          (for [j (range image-height)
+                               i (range image-width)]
+                           (->> #(let [pixel-sample  (-> pixel-00-loc
+                                                         (vec3/add (vec3/multiply pixel-du (+ i (- (rand) 0.5))))
+                                                         (vec3/add (vec3/multiply pixel-dv (+ j (- (rand) 0.5)))))
+                                       ray-direction (vec3/subtract pixel-sample camera-center)
+                                       a-ray         #::ray{:origin camera-center :direction ray-direction}]
+                                   (color a-ray hittables))
+                                (repeatedly samples-per-px)
+                                (reduce vec3/add)
+                                ((fn [color] (vec3/divide color samples-per-px)))))]
      (with-open [out (io/writer "scene.ppm")]
        (.write out (str "P3\n" image-width " " image-height "\n255\n"))
-       (doall
-        (for [j (range image-height)
-              i (range image-width)]
-          (let [pixel-center  (-> pixel-00-loc
-                                  (vec3/add (vec3/multiply pixel-du i))
-                                  (vec3/add (vec3/multiply pixel-dv j)))
-                ray-direction (vec3/subtract pixel-center camera-center)
-                a-ray         #::ray{:origin camera-center :direction ray-direction}
-                color         (color a-ray hittables)]
-            (write-color! out color)))))
+       (doseq [color colors]
+         (write-color! out color)))
      :done)))
