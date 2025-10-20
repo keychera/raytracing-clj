0840b53cd6bc027908a4cc088340e7bd8daf2783
keychera <kyogatama@gmail.com>
2025-10-20 23:13:34 +0900

    maybe a bad idea, but the sky is blue

--- src/experimental/raytracing_i.clj
+++ src/experimental/raytracing_i.clj
@@ -5,13 +5,87 @@
 
 (set! *warn-on-reflection* true)
 
+;; hmm maybe this is a bad idea... but I want to do it....
+(defn ray-color! [^doubles realm i> target ray-center ray-direction]
+  (-> realm
+      (vec3i/unit-vec3! (i> :temp) ray-direction)
+      (vec3i/closure-1
+       (i> :temp)
+       (fn [^doubles realm _ y _]
+         (let [a (* 0.5 (+ y 1.0))
+               r (+ (* (- 1.0 a) 1.0) (* a 0.5))
+               g (+ (* (- 1.0 a) 1.0) (* a 0.7))
+               b (+ (* (- 1.0 a) 1.0) (* a 1.0))]
+           (vec3i/create! realm target r g b))))))
+
+(defn create-i> [globals offset]
+  (into {} (map-indexed (fn [i v] [v (+ offset i)])) globals))
+
 (defn -main []
-  (let [image-width    256.0
-        image-height   256.0
-        ^doubles realm (make-array Double/TYPE (* image-width image-height 3))]
+  (let [aspect-ratio    16/9
+        image-width     400
+        image-height    (int (/ image-width aspect-ratio))
+
+        focal-length    1.0
+        viewport-height 2.0
+        viewport-width  (* viewport-height (/ image-width image-height))
+
+        pixel-count     (* image-width image-height)
+        i>              (create-i>
+                         [;; global vectors
+                          :camera-center
+                          :viewport-u
+                          :viewport-v
+                          :pixel-du
+                          :pixel-dv
+                          :upper-left
+                          :pixel-00
+
+                          ;; loop vectors
+                          :pixel-center
+                          :ray-direction
+                          :pixel-color
+
+                          :temp]
+                         pixel-count)
+
+        realm-size      (* (+ pixel-count (count i>)) 3)
+        ^doubles realm  (make-array Double/TYPE realm-size)]
+
+    (-> realm
+        (vec3i/create! (i> :camera-center) 0.0 0.0 -1.0)
+        (vec3i/create! (i> :viewport-u) viewport-width 0.0 0.0)
+        (vec3i/create! (i> :viewport-v) 0.0 (- viewport-height) 0.0)
+        (vec3i/divide! (i> :pixel-du) (i> :viewport-u) image-width)
+        (vec3i/divide! (i> :pixel-dv) (i> :viewport-v) image-height)
+
+        ;; multiple operand is complex to represent currently 
+        (vec3i/create! (i> :temp) 0.0 0.0 focal-length)
+        (vec3i/subtract! (i> :upper-left) (i> :camera-center) (i> :temp))
+        (vec3i/divide! (i> :temp) (i> :viewport-u) 2.0)
+        (vec3i/subtract! (i> :upper-left) (i> :upper-left) (i> :temp))
+        (vec3i/divide! (i> :temp) (i> :viewport-v) 2.0)
+        (vec3i/subtract! (i> :upper-left) (i> :upper-left) (i> :temp))
+
+        (vec3i/add! (i> :temp) (i> :pixel-du) (i> :pixel-dv))
+        (vec3i/divide! (i> :temp) (i> :temp) 2.0)
+        (vec3i/add! (i> :pixel-00) (i> :upper-left) (i> :temp)))
+
     (dotimes [j image-height]
       (dotimes [i image-width]
-        (vec3i/create! realm (long (+ i (* j image-width))) (/ i image-width) (/ j image-width) 0.0)))
+        (-> realm
+            (vec3i/copy! (i> :pixel-center) (i> :pixel-00))
+            (vec3i/mult-scalar! (i> :temp) (i> :pixel-du) i)
+            (vec3i/add! (i> :pixel-center) (i> :pixel-center) (i> :temp))
+            (vec3i/mult-scalar! (i> :temp) (i> :pixel-dv) j)
+            (vec3i/add! (i> :pixel-center) (i> :pixel-center) (i> :temp))
+
+            (vec3i/subtract! (i> :ray-direction) (i> :pixel-center) (i> :camera-center))
+
+            (ray-color! i> (i> :pixel-color) (i> :camera-center) (i> :ray-direction))
+
+            (vec3i/copy! (long (+ i (* j image-width))) (i> :pixel-color)))))
+
     (with-open [out (io/writer "scene-i.ppm")]
       (.write out (str "P3\n" image-width " " image-height "\n255\n"))
       (dotimes [j image-height]
--- src/experimental/vec3i.clj
+++ src/experimental/vec3i.clj
@@ -4,20 +4,23 @@
 (defmacro y ^double [^doubles realm i] `(aget ~realm (+ 1 (* ~i 3))))
 (defmacro z ^double [^doubles realm i] `(aget ~realm (+ 2 (* ~i 3))))
 
-(defmacro create! [^doubles realm i x y z]
+(defmacro create! [^doubles realm target x y z]
   `(doto ~realm
-     (aset (* ~i 3) ~x)
-     (aset (+ 1 (* ~i 3)) ~y)
-     (aset (+ 2 (* ~i 3)) ~z)))
+     (aset (* ~target 3) (double ~x))
+     (aset (+ 1 (* ~target 3)) (double ~y))
+     (aset (+ 2 (* ~target 3)) (double ~z))))
 
-(defmacro operator [op ^doubles realm u v target]
+(defmacro operator [op ^doubles realm target u v]
   `(let [u0# (x ~realm ~u) u1# (y ~realm ~u) u2# (z ~realm ~u)
          v0# (x ~realm ~v) v1# (y ~realm ~v) v2# (z ~realm ~v)]
      (create! ~realm ~target (~op u0# v0#) (~op u1# v1#) (~op u2# v2#))))
 
-(defmacro operator-scalar [op ^doubles realm v scalar target]
+(defmacro operator-scalar [op ^doubles realm target v scalar]
   `(let [x# (x ~realm ~v) y# (y ~realm ~v) z# (z ~realm ~v)]
      (create! ~realm ~target (~op x# ~scalar) (~op y# ~scalar) (~op z# ~scalar))))
+
+(defn closure-1 [^doubles realm u1 closure-fn]
+  (closure-fn realm (x realm u1) (y realm u1) (z realm u1)))
 
 ;; return scalar
 (defn length-squared [^doubles realm v]
@@ -32,23 +35,26 @@
      (* (y realm u) (y realm v))
      (* (z realm u) (z realm v))))
 
-;; mutate value on the realm, return realm
-(defn add! [^doubles realm u v target]
-  (operator + realm u v target))
+;; mutate value on the realm, return mutated realm
+(defn copy! [^doubles realm target u]
+  (create! realm target (x realm u) (y realm u) (z realm u)))
 
-(defn subtract! [^doubles realm u v target]
-  (operator - realm u v target))
+(defn add! [^doubles realm target u v]
+  (operator + realm target u v))
 
-(defn mult-vec3! [^doubles realm u v target]
-  (operator * realm u v target))
+(defn subtract! [^doubles realm target u v]
+  (operator - realm target u v))
 
-(defn mult-scalar! [^doubles realm v scalar target]
-  (operator-scalar * realm v scalar target))
+(defn mult-vec3! [^doubles realm target u v]
+  (operator * realm target u v))
 
-(defn divide! [^doubles realm v scalar target]
-  (operator-scalar / realm v scalar target))
+(defn mult-scalar! [^doubles realm target v scalar]
+  (operator-scalar * realm target v scalar))
 
-(defn cross! [^doubles realm u v target]
+(defn divide! ^doubles [^doubles realm target v scalar]
+  (operator-scalar / realm target v scalar))
+
+(defn cross! [^doubles realm target u v]
   (let [u0 (x realm u) u1 (y realm u) u2 (z realm u)
         v0 (x realm v) v1 (y realm v) v2 (z realm v)]
     (create! realm target
@@ -56,9 +62,9 @@
              (- (* u2 v0) (* u0 v2))
              (- (* u0 v1) (* u1 v0)))))
 
-(defn unit-vec3! [^doubles realm v target]
+(defn unit-vec3! [^doubles realm target v]
   (-> realm
-      (divide! v (length realm v) target)))
+      (divide! target v (length realm v))))
 
 
 ;; take a vec3 from the realm
