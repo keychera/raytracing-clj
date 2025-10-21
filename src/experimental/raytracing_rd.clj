4d2ebf3fa147d018d7e4f18d2710546e50a53a3c
keychera <kyogatama@gmail.com>
2025-10-21 23:17:14 +0900

    use qualified keyword

--- src/experimental/raytracing_rd.clj
+++ src/experimental/raytracing_rd.clj
@@ -4,7 +4,7 @@
    [clojure.java.io :as io]
    [clojure.math :as math]
    [experimental.vec3_realm]
-   [criterium.core :as criterium])
+   #_[criterium.core :as criterium])
   (:import [experimental.vec3_realm Realm]))
 
 (set! *warn-on-reflection* true)
@@ -23,10 +23,10 @@
 (deftype Sphere [center-i ^double radius]
   Hittable
   (^double hit [_this ^experimental.vec3_realm.Realm realm i> ray-origin ray-direction ^double t-min ^double t-max]
-    (doto realm (.subtract (i> :temp) center-i ray-origin))
+    (doto realm (.subtract (i> ::temp) center-i ray-origin))
     (let [a (.lengthSquared realm ray-direction)
-          h (.dot realm ray-direction (i> :temp))
-          c (- (.dot realm (i> :temp) (i> :temp)) (* radius radius))
+          h (.dot realm ray-direction (i> ::temp))
+          c (- (.dot realm (i> ::temp) (i> ::temp)) (* radius radius))
           discriminant (- (* h h) (* a c))]
       (if (< discriminant 0.0)
         -1.0
@@ -38,14 +38,14 @@
           (if (or (<= root t-min) (<= t-max root))
             -1.0
             (do (doto realm
-                  (ray-at (i> :hit-point) ray-origin ray-direction root)
-                  (.subtract (i> :temp) (i> :hit-point) center-i)
-                  (.divideScalar (i> :hit-normal) (i> :temp) radius))
-                (let [front-face? (< (.dot realm ray-direction (i> :hit-normal)) 0)]
+                  (ray-at (i> ::hit-point) ray-origin ray-direction root)
+                  (.subtract (i> ::temp) (i> ::hit-point) center-i)
+                  (.divideScalar (i> ::hit-normal) (i> ::temp) radius))
+                (let [front-face? (< (.dot realm ray-direction (i> ::hit-normal)) 0)]
                   (when (not front-face?)
-                    (.multScalar realm (i> :hit-normal) (i> :hit-normal) -1)))
+                    (.multScalar realm (i> ::hit-normal) (i> ::hit-normal) -1)))
                 root)))))
-    #_"this mutates :hit-normal and :hit-point"))
+    #_"this mutates ::hit-normal and ::hit-point"))
 
 (defn hit-anything! [realm i> ray-origin ray-direction hittables t-min t-max]
   (let [length (count hittables)]
@@ -63,12 +63,12 @@
         t     (double (hit-anything! realm i> ray-origin ray-direction hittables 1e-3 ##Inf))]
     (if (> t 0.0)
       (doto realm
-        (.create (i> :temp) 1.0 1.0 1.0)
-        (.add (i> :temp) (i> :hit-normal) (i> :temp))
-        (.multScalar (i> :temp) (i> :temp) 0.5)
-        (.copy target (i> :temp)))
-      (do (.unitVec3 realm (i> :temp) ray-direction)
-          (let [y (.y realm (i> :temp))
+        (.create (i> ::temp) 1.0 1.0 1.0)
+        (.add (i> ::temp) (i> ::hit-normal) (i> ::temp))
+        (.multScalar (i> ::temp) (i> ::temp) 0.5)
+        (.copy target (i> ::temp)))
+      (do (.unitVec3 realm (i> ::temp) ray-direction)
+          (let [y (.y realm (i> ::temp))
                 a (* 0.5 (+ y 1.0))
                 r (+ (* (- 1.0 a) 1.0) (* a 0.5))
                 g (+ (* (- 1.0 a) 1.0) (* a 0.7))
@@ -81,7 +81,7 @@
 
 (defn -main []
   (do #_#_prof/profile
-   {:event :alloc}
+        {:event :alloc}
    (let [aspect-ratio    (double 16/9)
          image-width     400
          image-height    (int (/ image-width aspect-ratio))
@@ -93,22 +93,22 @@
          viewport-width  (* viewport-height (double (/ image-width image-height)))
 
          pixel-count     (* image-width image-height)
-         global-vecs     [:camera-center
-                          :viewport-u
-                          :viewport-v
-                          :pixel-du
-                          :pixel-dv
-                          :upper-left
-                          :pixel-00]
+         global-vecs     [::camera-center
+                          ::viewport-u
+                          ::viewport-v
+                          ::pixel-du
+                          ::pixel-dv
+                          ::upper-left
+                          ::pixel-00]
+         spheres         [::sphere-1
+                          ::sphere-2]
          loop-vecs       [::pixel-sample
                           ::ray-origin
                           ::ray-direction
                           ::pixel-color]
-         spheres         [:sphere-1
-                          :sphere-2]
-         temporaries     [:temp
-                          :hit-point
-                          :hit-normal]
+         temporaries     [::temp
+                          ::hit-point
+                          ::hit-normal]
          i>              (create-i>
                           (concat global-vecs
                                   loop-vecs
@@ -118,46 +118,46 @@
          realm-size      (* (+ pixel-count (count i>)) 3)
          ^Realm realm    (Realm. (make-array Double/TYPE realm-size))
 
-         sphere-1        (let [circle-i (i> :sphere-1)]
+         sphere-1        (let [circle-i (i> ::sphere-1)]
                            (.create realm circle-i 0.0 0.0 -1.0)
                            {:hittable (Sphere. circle-i 0.5)})
-         sphere-2        (let [circle-i (i> :sphere-2)]
+         sphere-2        (let [circle-i (i> ::sphere-2)]
                            (.create realm circle-i 0.0 -100.5 -1.0)
                            {:hittable (Sphere. circle-i 100.0)})
          hittables       [sphere-1 sphere-2]]
 
-     (criterium/bench
+     (time #_criterium/bench
       (do (doto realm
-            (.create (i> :camera-center) 0.0 0.0 0.0)
-            (.create (i> :viewport-u) viewport-width 0.0 0.0)
-            (.create (i> :viewport-v) 0.0 (- viewport-height) 0.0)
-            (.divideScalar (i> :pixel-du) (i> :viewport-u) image-width)
-            (.divideScalar (i> :pixel-dv) (i> :viewport-v) image-height)
+            (.create (i> ::camera-center) 0.0 0.0 0.0)
+            (.create (i> ::viewport-u) viewport-width 0.0 0.0)
+            (.create (i> ::viewport-v) 0.0 (- viewport-height) 0.0)
+            (.divideScalar (i> ::pixel-du) (i> ::viewport-u) image-width)
+            (.divideScalar (i> ::pixel-dv) (i> ::viewport-v) image-height)
 
             ;; multiple operand is complex to represent currently 
-            (.create (i> :temp) 0.0 0.0 focal-length)
-            (.subtract (i> :upper-left) (i> :camera-center) (i> :temp))
-            (.divideScalar (i> :temp) (i> :viewport-u) 2.0)
-            (.subtract (i> :upper-left) (i> :upper-left) (i> :temp))
-            (.divideScalar (i> :temp) (i> :viewport-v) 2.0)
-            (.subtract (i> :upper-left) (i> :upper-left) (i> :temp))
+            (.create (i> ::temp) 0.0 0.0 focal-length)
+            (.subtract (i> ::upper-left) (i> ::camera-center) (i> ::temp))
+            (.divideScalar (i> ::temp) (i> ::viewport-u) 2.0)
+            (.subtract (i> ::upper-left) (i> ::upper-left) (i> ::temp))
+            (.divideScalar (i> ::temp) (i> ::viewport-v) 2.0)
+            (.subtract (i> ::upper-left) (i> ::upper-left) (i> ::temp))
 
-            (.add (i> :temp) (i> :pixel-du) (i> :pixel-dv))
-            (.divideScalar (i> :temp) (i> :temp) 2.0)
-            (.add (i> :pixel-00) (i> :upper-left) (i> :temp)))
+            (.add (i> ::temp) (i> ::pixel-du) (i> ::pixel-dv))
+            (.divideScalar (i> ::temp) (i> ::temp) 2.0)
+            (.add (i> ::pixel-00) (i> ::upper-left) (i> ::temp)))
 
           (dotimes [j image-height]
             (dotimes [i image-width]
               (.create realm (i> ::pixel-color) 0.0 0.0 0.0)
               (dotimes [_ samples-per-px]
                 (doto realm
-                  (.copy (i> ::pixel-sample) (i> :pixel-00))
-                  (.multScalar (i> :temp) (i> :pixel-du) (+ i (- (math/random) 0.5)))
-                  (.add (i> ::pixel-sample) (i> ::pixel-sample) (i> :temp))
-                  (.multScalar (i> :temp) (i> :pixel-dv) (+ j (- (math/random) 0.5)))
-                  (.add (i> ::pixel-sample) (i> ::pixel-sample) (i> :temp))
+                  (.copy (i> ::pixel-sample) (i> ::pixel-00))
+                  (.multScalar (i> ::temp) (i> ::pixel-du) (+ i (- (math/random) 0.5)))
+                  (.add (i> ::pixel-sample) (i> ::pixel-sample) (i> ::temp))
+                  (.multScalar (i> ::temp) (i> ::pixel-dv) (+ j (- (math/random) 0.5)))
+                  (.add (i> ::pixel-sample) (i> ::pixel-sample) (i> ::temp))
 
-                  (.copy (i> ::ray-origin) (i> :camera-center))
+                  (.copy (i> ::ray-origin) (i> ::camera-center))
                   (.subtract (i> ::ray-direction) (i> ::pixel-sample) (i> ::ray-origin))
 
                   ;; pixel-sample is used as temp, this is some bug-prone way of programming huh
